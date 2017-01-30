f166827f9bf6aecba122ec154a7e49acc00e8af4
yionlee <performizer@gmail.com>
2017-01-30 20:05:43 +0900

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+/**
+ * stateless functional component 방식 : state가 없거나 life cycle 필요 없는 컴포넌트의 경우 이러한 형태로 정의한다
+ */
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+};
+    
+export default Home;
